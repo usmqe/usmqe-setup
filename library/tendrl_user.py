fb2aaec186b5ad8acf674e75b1956497b7f97689
Martin Kudlej <mkudlej@redhat.com>
2017-03-28 13:58:49 +0200

    add new password as argument and add playbook

--- library/tendrl_user.py
+++ library/tendrl_user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 '''
 module: test_user
-short_description: Changes 'usm_username' user password to 'admin123' via Tendrl API.
+short_description: Changes 'usm_username' user password to _password_from_module_argument_
+via Tendrl API.
 description:
     - Module reads from confile(module argument 'conf_path') 'usm_username',
       'usm_password' and 'usm_api_url'.
@@ -22,19 +23,21 @@ ANSIBLE_METADATA = {'metadata_version': '1.0',
 
 DOCUMENTATION = '''
 module: test_user
-short_description: Changes 'usm_username' user password to 'admin123' via Tendrl API.
+short_description: Changes 'usm_username' user password to _password_from_module_argument_
+via Tendrl API.
 description:
     - Module reads from confile(module argument 'conf_path') 'usm_username', 
       'usm_password' and 'usm_api_url'.
     - There must be installed tendrl-api on the machine where this is ran.
 version_added: "1.0"
+    - Usage: ansible -i _inventory_ -m tendrl_user -a "conf_path='_usm.ini.path_'" localhost
 author: "fbalak@redhat.com", "mkudlej@redhat.com"
 '''
 
-NEW_PASSWORD = "admin123"
 MODULE = AnsibleModule(
     argument_spec=dict(
-        conf_path=dict(required=True, type='str')
+        conf_path=dict(required=True, type='str'),
+        new_password=dict(required=True, type='str')
         )
     )
 
@@ -76,6 +79,7 @@ def main():
     """Main function"""
 
     conf_path = MODULE.params['conf_path']
+    new_password = MODULE.params['new_password']
     config = ConfigParser.SafeConfigParser()
     config.read(conf_path)
 
@@ -84,15 +88,17 @@ def main():
     api_url = config.get('usmqepytest', 'usm_api_url')
 
     token = login(api_url, username, password)["access_token"]
-    result = change_password(api_url, username, NEW_PASSWORD, token)
+    if password == new_password:
+        MODULE.exit_json(changed=False)
+
+    result = change_password(api_url, username, new_password, token)
     response = {"result": result}
 
-    config.set('usmqepytest', 'usm_password', NEW_PASSWORD)
+    config.set('usmqepytest', 'usm_password', new_password)
     with open(conf_path, 'wb') as conffile:
         config.write(conffile)
 
     MODULE.exit_json(changed=True, meta=response)
-
 
 if __name__ == '__main__':
     main()
