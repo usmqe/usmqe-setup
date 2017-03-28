e0a6600b203174ef2965d155c13b31598e6f6a20
Martin Kudlej <mkudlej@redhat.com>
2017-03-28 13:39:14 +0200

    Chnage tendrl_user module. Now module only changes password for user 'usm_username' from 'usm_password' to 'admin123'. module requires one argument path to usm.ini file.

--- library/tendrl_user.py
+++ library/tendrl_user.py
@@ -1,98 +1,97 @@
 #!/usr/bin/python
+'''
+module: test_user
+short_description: Changes 'usm_username' user password to 'admin123' via Tendrl API.
+description:
+    - Module reads from confile(module argument 'conf_path') 'usm_username',
+      'usm_password' and 'usm_api_url'.
+    - There must be installed tendrl-api on the machine where this is ran.
+    - Usage: ansible -i _inventory_ -m tendrl_user -a "conf_path='_usm.ini.path_'" localhost
+version_added: "1.0"
+author: "fbalak@redhat.com", "mkudlej@redhat.com"
+'''
+
+import json
+import ConfigParser
+import requests
+from ansible.module_utils.basic import AnsibleModule
+
 ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
 DOCUMENTATION = '''
 module: test_user
-short_description: Creates user admin with password admin123 for tendrl.
+short_description: Changes 'usm_username' user password to 'admin123' via Tendrl API.
 description:
-    - Creates user admin with password admin123 for tendrl
+    - Module reads from confile(module argument 'conf_path') 'usm_username', 
+      'usm_password' and 'usm_api_url'.
     - There must be installed tendrl-api on the machine where this is ran.
 version_added: "1.0"
-author: "fbalak@redhat.com"
+author: "fbalak@redhat.com", "mkudlej@redhat.com"
 '''
 
-from ansible.module_utils.basic import AnsibleModule
-import re
-import requests
-import os
-import socket
-import json
+NEW_PASSWORD = "admin123"
+MODULE = AnsibleModule(
+    argument_spec=dict(
+        conf_path=dict(required=True, type='str')
+        )
+    )
 
-hostname = socket.gethostname()
-module = AnsibleModule(argument_spec={})
-new_password = "admin123"
-
-
-def generate_user():
-    """Generates admin according to:
-    https://github.com/Tendrl/api/blob/master/docs/users.adoc#create-admin-user"""
-
-    os.chdir("/usr/share/tendrl-api")
-    return os.popen("RACK_ENV=production rake etcd:load_admin").read()
-
-
-def parse_password(string):
-    """Gets password from user_info. If there is none then it tries to login
-    with default password."""
-
-    password = re.compile("\"Password:\s*(?P<pass>[a-zA-Z0-9\._]+)\"")
-    admin_exists = re.compile("User named admin already exists.")
-    password_found = password.search(string)
-    admin_found = admin_exists.search(string)
-    if password_found is not None:
-        return password_found.group("pass")
-    elif admin_found is not None:
-        login("admin", new_password)
-        response = {"result": "User admin with password {} already exists."
-                    .format(new_password)}
-        module.exit_json(changed=False, meta=response)
-
-
-def login(username, password):
+def login(api_url, username, password):
     """Login to tendrl server."""
 
     post_data = {"username": username, "password": password}
     request = requests.post(
-        "http://{}/api/1.0/login".format(hostname),
+        "{}login".format(api_url),
         data=json.dumps(post_data))
-    if request.status_code == requests.codes.ok:
+    if request.status_code == requests.codes["ok"]:
         return request.json()
     else:
         response = {"url": request.url, "data": post_data}
-        module.fail_json(
+        MODULE.fail_json(
             msg="Could not login with these credentials.",
             meta=response)
 
-
-def change_password(username, password, token):
+def change_password(api_url, username, password, token):
     """Change password of user and set it's email to username@hostname"""
 
     post_data = {
         "username": username,
         "password": password,
         "password_confirmation": password,
-        "email": "{}@{}".format(username, hostname)}
+        "email": "{}@{}".format(username, 'localhost.localdomain')}
     request = requests.put(
-        "http://{}/api/1.0/users/{}".format(hostname, username),
+        "{}users/{}".format(api_url, username),
         data=json.dumps(post_data),
         headers={"Authorization": "Bearer {}".format(token)})
-    if request.status_code == requests.codes.ok:
+    if request.status_code == requests.codes["ok"]:
         return request.json()
     else:
-        response = {"url": request.url, "data": post_data, "token": token}
-        module.fail_json(msg="Request to server failed.", meta=response)
+        response = {"url": request.url, "data": post_data, "token": token, "text": request.text}
+        MODULE.fail_json(msg="Request to server failed.", meta=response)
 
 
 def main():
-    username = "admin"
-    user_info = str(generate_user())
-    password = parse_password(user_info)
-    token = login(username, password)["access_token"]
-    result = change_password(username, new_password, token)
+    """Main function"""
+
+    conf_path = MODULE.params['conf_path']
+    config = ConfigParser.SafeConfigParser()
+    config.read(conf_path)
+
+    username = config.get('usmqepytest', 'usm_username')
+    password = config.get('usmqepytest', 'usm_password')
+    api_url = config.get('usmqepytest', 'usm_api_url')
+
+    token = login(api_url, username, password)["access_token"]
+    result = change_password(api_url, username, NEW_PASSWORD, token)
     response = {"result": result}
-    module.exit_json(changed=True, meta=response)
+
+    config.set('usmqepytest', 'usm_password', NEW_PASSWORD)
+    with open(conf_path, 'wb') as conffile:
+        config.write(conffile)
+
+    MODULE.exit_json(changed=True, meta=response)
 
 
 if __name__ == '__main__':
